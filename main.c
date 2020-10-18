5bb91583f561fe15cf8fc21b8f94ba7c00b6dc13
Denis Zagorodskii <zagorodskiydenis@gmail.com>
2020-10-18 20:41:03 +0900

    Create menu in main function

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#define LEN 100
 
 main(){
-	
+	char c;
+	FILE *tf;
+	while (1){
+		//clrscr();
+		puts(" 1 - New file");
+		puts(" 2 - View file");
+		puts(" 3 - Add to file");
+		puts(" 4 - Find and Change");
+		puts(" 5 - Swap 1 and 2 athlete");
+		puts(" 0 - Exit");
+		c=getch();
+		switch(c){
+			case '1':;break;
+			case '2':;break;
+			case '3':;break;
+			case '4':;break;
+			case '5':;break;
+			case '0':return(0);
+			default: puts("\n Neverniy regim!");
+		}
+	}
 }
