781a5d72d21da6b1f91c85611e59cd2e644d1720
Denis Zagorodskii <zagorodskiydenis@gmail.com>
2020-10-18 21:41:09 +0900

    Create function print

---
+++ print.c
@@ -0,0 +1,16 @@
+#include "variables.c"
+
+void print(FILE *tf){
+	int i;
+	system("CLS");//clrscr();
+	tf=fopen("athletes.dat", "rb");//Открытие бинарного файла для чтения
+	i=1;
+	fread(&t1, sizeof(t1), 1, tf);//Чтение из файла одной структуры t1
+	while (!feof(tf)){
+		printf("\n %6.2d athlete %10s \t number %3d \t score %3f", i, t1.surname, t1.number, t1.score);
+		fread(&t1, sizeof(t1), 1, tf);
+		i++;
+	}
+	getch();
+	system("CLS");
+}
