a1778dc937c357cbaf817218df02e6f3117121e2
Denis Zagorodskii <zagorodskiydenis@gmail.com>
2020-11-01 03:12:32 +0900

    Minor changes

--- add.c
+++ add.c
@@ -7,14 +7,14 @@ void add(FILE *tf, int n){
 	system("CLS");
 	if(n==0){
 		printf("\n Input data \n");
-			do {
-				printf("\n Surname: "); scanf("%s", &t1.surname);
-				printf("\n Number: "); scanf("%d", &t1.number);
-				printf("\n Score: "); scanf("%f", &t1.score);
-				fwrite(&t1, sizeof(t1), 1, tf);
-				printf("\n Finish? y/n ");
-				ch=getch();
-			}
+		do {
+			printf("\n Surname: "); scanf("%s", &t1.surname);
+			printf("\n Number: "); scanf("%d", &t1.number);
+			printf("\n Score: "); scanf("%f", &t1.score);
+			fwrite(&t1, sizeof(t1), 1, tf);
+			printf("\n Finish? y/n ");
+			ch=getch();
+		}
 		while (ch!='y');
 		fclose(tf);
 		system("CLS");
